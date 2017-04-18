0e9ca70b52cd3bffb737c18c5027ce9517118ea5
Tom Lavenziano <TomLavenziano@Triangulr.io>
2017-04-17 21:30:56 -0400

    Changed models and controllers to camelCase, added mongo-sails to package.json

--- api/models/users.js
+++ api/models/users.js
@@ -1,6 +1,5 @@
 /**
- * Users
- .js
+ * Users.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
