268b6ed2afe134d69e3572255cd64b6eb8bfa8bf
ChristopherStolz <cstolz@gmail.com>
2017-04-20 04:41:13 +0800

    Seriously tho, fullcalendar doesn't work

---
+++ assets/js/schedule.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    defaultView:'timelineMonth',
+    events:[
+    //events
+    ],
+    resources:[
+    //resources
+    ]
+  });  
+});
