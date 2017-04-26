cb4a9520834aabad351825474fd95ef7401aee41
ChristopherStolz <cstolz@gmail.com>
2017-04-27 03:13:25 +0800

    Add events onDayClick works.

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
   $('#calendar').fullCalendar({
-    // defaultView:'month',
+    defaultView:'month',
     header: {
       left:'month,agendaWeek,agendaDay',
       center:'title',
       right:'today prev,next'
     },
     dayClick: function(date, jsEvent, view, allDay) {
-      if(view.name != 'month')
-      return;
-      $('#calendar').fullCalendar('changeView', 'agendaDay', date);
+      var abc = prompt("Event title: ");
+      var newEvent = {
+        title: abc,
+        start: date.format()
+      }
+      $('#calendar').fullCalendar('renderEvent', newEvent);
+      // if(view.name != 'month')
+      // return;
+      // $('#calendar').fullCalendar('changeView', 'agendaDay', date);
     },
-    // eventClick: function(calEvent, jsEvent, view){
-    //   alert('Event' + cal.Event.title);
+     eventClick: function(calEvent, jsEvent, view){
+       alert('Event: ' + calEvent.title);
     //   //TODO display all info for event
     //
-    // },
+     },
     events:[
 
     ],
